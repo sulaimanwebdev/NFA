d1a24179eb7d23e721dbc6ce1a0e17b359db5152
Muhammad Sulaiman <sulaimanperson@gmail.com>
2021-12-19 18:38:57 +0500

    alright

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ let minusBtn = document.querySelector('.minusBtn');
 let plusBtn = document.querySelector('.plusBtn');
 
 let count = 1;
-totalPrice.textContent = 0.15;
+totalPrice.textContent = ".014";
 plusBtn.addEventListener('click', () =>{
 if(count < 10){
     count+=1;
@@ -92,45 +92,45 @@ count = 10;
 }
 
 if(count === 1){
-    totalPrice.textContent = "0.15";
+    totalPrice.textContent = ".014";
 }
 
 if(count === 2){
-    totalPrice.textContent = "0.35";
+    totalPrice.textContent = ".028";
 }
 
 if(count === 3){
-    totalPrice.textContent = "0.45";
+    totalPrice.textContent = ".042";
 }
 
 if(count === 4){
-    totalPrice.textContent = "0.60";
+    totalPrice.textContent = ".056";
 }
 
 if(count === 5){
-    totalPrice.textContent = "0.75";
+    totalPrice.textContent = ".070";
 }
 
 
 if(count === 6){
-    totalPrice.textContent = "0.90";
+    totalPrice.textContent = ".084";
 }
 
 if(count === 7){
-    totalPrice.textContent = "1.5";
+    totalPrice.textContent = ".098";
 }
 
 if(count === 8){
-    totalPrice.textContent = "1.15";
+    totalPrice.textContent = ".112";
 }
 
 if(count === 9){
-    totalPrice.textContent = "1.35";
+    totalPrice.textContent = ".126";
 }
 
 
 if(count === 10){
-    totalPrice.textContent = "1.50";
+    totalPrice.textContent = ".140";
 }
 
 })
@@ -153,45 +153,45 @@ minusBtn.addEventListener('click', () =>{
 }
 
  if(count === 1){
-    totalPrice.textContent = "0.15";
+    totalPrice.textContent = ".014";
 }
 
 if(count === 2){
-    totalPrice.textContent = "0.35";
+    totalPrice.textContent = ".028";
 }
 
 if(count === 3){
-    totalPrice.textContent = "0.45";
+    totalPrice.textContent = ".042";
 }
 
 if(count === 4){
-    totalPrice.textContent = "0.60";
+    totalPrice.textContent = ".056";
 }
 
 if(count === 5){
-    totalPrice.textContent = "0.75";
+    totalPrice.textContent = ".070";
 }
 
 
 if(count === 6){
-    totalPrice.textContent = "0.90";
+    totalPrice.textContent = ".084";
 }
 
 if(count === 7){
-    totalPrice.textContent = "1.5";
+    totalPrice.textContent = ".098";
 }
 
 if(count === 8){
-    totalPrice.textContent = "1.15";
+    totalPrice.textContent = ".112";
 }
 
 if(count === 9){
-    totalPrice.textContent = "1.35";
+    totalPrice.textContent = ".126";
 }
 
 
 if(count === 10){
-    totalPrice.textContent = "1.50";
+    totalPrice.textContent = ".140";
 }
 
 
